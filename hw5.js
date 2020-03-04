afb572f3eccc523ffd8dcf419a0165de4e96ae51
Jane <cjmendez26@gmail.com>
2020-03-03 19:08:27 -0800

    fixing the js file that I erased

---
+++ hw5.js
@@ -0,0 +1,26 @@
+function renderTimer() {
+    $("#clockDisplay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+}
+renderTimer();
+
+// function colorMorph() {
+//     var hour = moment().startOf('day').fromNow();
+//     var currentHour = parseInt(hour.split(" ")[0]);
+//  $(".save").each(function(){
+    
+//     var buttonHour = $(this).attr("id") / 100;
+
+//     console.log(typeof currentHour, typeof buttonHour)
+//   if (currentHour === buttonHour ) {
+//       $(this).removeClass().addClass("btn bg-danger btn-outline-secondary save");
+// // } else if (currentHour <= buttonHour) {
+// //   $(this).removeClass().addClass("btn bg-warning btn-outline-secondary save");
+// // } else (currentHour >= buttonHour) {
+// //   $(this).removeClass().addClass("btn bg-success btn-outline-secondary save");
+// // }
+//     //  $(this).removeClass().addClass("btn bg-warning btn-outline-secondary save");
+// //  })
+// }
+
+// colorMorph();
+

b04a6ce29477f6dd9310bf85bb87753fe62417a6
Jane <cjmendez26@gmail.com>
2020-03-22 21:20:40 -0700

    local storega

--- index.js
+++ index.js
@@ -3,23 +3,33 @@ function renderTimer() {
 }
 renderTimer();
 
-// function colorMorph() {
-//     var hour = moment().startOf('day').fromNow();
-//     var currentHour = parseInt(hour.split(" ")[0]);
-//  $('.save').each(function(){
-    
-//     var buttonHour = $(this).attr('id') / 100;
+function colorMorph() {
+    var hour = moment().startOf('day').fromNow();
+    var currentHour = parseInt(hour.split(" ")[0]);
+ $('.save').each(function(){
 
-//     console.log(typeof currentHour, typeof buttonHour);
-// }
-// colorMorph();
-//   if (currentHour === buttonHour ) {
-//       $(this).removeClass().addClass("btn bg-danger btn-outline-secondary save");
-// // } else if (currentHour <= buttonHour) {
-// //   $(this).removeClass().addClass("btn bg-warning btn-outline-secondary save");
-// // } else (currentHour >= buttonHour) {
-// //   $(this).removeClass().addClass("btn bg-success btn-outline-secondary save");
-// // }
-//     //  $(this).removeClass().addClass("btn bg-warning btn-outline-secondary save");
-// //  })
-// }
+     
+     var buttonHour = $(this).attr('id') / 100;
+     $(this).click(function(){
+        console.log(buttonHour);
+        var key = "hour-" + buttonHour;
+        var value = $("#input-"+ buttonHour).val();
+        console.log(value);
+        window.localStorage.setItem(key, value)
+     })
+     
+    if (currentHour === buttonHour ) {
+        $(this).removeClass().addClass("btn bg-danger btn-outline-secondary save");
+  } else if (currentHour <= buttonHour) {
+    $(this).removeClass().addClass("btn bg-warning btn-outline-secondary save");
+  } else {
+    $(this).removeClass().addClass("btn bg-success btn-outline-secondary save");
+  }
+
+})}
+
+colorMorph();
+
+
+
+  
